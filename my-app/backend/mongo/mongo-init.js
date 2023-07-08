f6e00389a93f718c6e677e3b271443e897d6a9ac
Enrique Verea <kikeverea@gmail.com>
2023-07-08 19:22:11 +0200

    Change mongo-init to create a db for each env

--- my-app/backend/mongo/mongo-init.js
+++ my-app/backend/mongo/mongo-init.js
@@ -1,16 +1,37 @@
-db.createUser({
-  user: 'dev',
-  pwd: 'pass',
-  roles: [
-    {
-      role: 'dbOwner',
-      db: 'dev_database',
-    },
-  ],
-});
+print('Start #################################################################');
 
+db = db.getSiblingDB('prod_db');
+db.createUser(
+  {
+    user: 'admin',
+    pwd: 'pass',
+    roles: [
+      {
+        role: 'dbOwner',
+        db: 'prod_db'
+      }
+    ],
+  },
+);
 db.createCollection('blogs');
 db.createCollection('users');
 db.createCollection('comments');
 
+db = db.getSiblingDB('dev_db');
+db.createUser(
+  {
+    user: 'dev',
+    pwd: 'pass',
+    roles: [
+      {
+        role: 'dbOwner',
+        db: 'dev_db'
+      }
+    ],
+  },
+);
+db.createCollection('blogs');
+db.createCollection('users');
+db.createCollection('comments');
 
+print('END #################################################################')
